573e094ac0bc2c5697bb1e5216057021fd725063
Camm07 <127072716+Camm07@users.noreply.github.com>
2024-05-28 23:38:19 -0700

    actualizaciones

--- InicioSocio.js
+++ InicioSocio.js
@@ -10,11 +10,6 @@ function toggleMenu() {
     }
 }
 
-function cerrarSesion() {
-    if (confirm("¿Estás seguro de que quieres cerrar sesión?")) {
-        window.location.href = 'index.html';
-    }
-}
 
 window.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.menu a').forEach(link => {
--- InicioVerSocio.js
+++ InicioVerSocio.js
@@ -10,12 +10,6 @@ function toggleMenu() {
     }
 }
 
-function cerrarSesion() {
-    if (confirm("¿Estás seguro de que quieres cerrar sesión?")) {
-        window.location.href = 'index.html';
-    }
-}
-
 window.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.menu a').forEach(link => {
         link.addEventListener('click', toggleMenu); // Añade cerrar menú al hacer clic en un enlace
